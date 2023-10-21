8705ce1c60e0d87cb4bd4a9727fdd47eb9f08057
zhyunk <gimwlgus@gmail.com>
2023-10-22 01:08:53 +0900

    05. if 학습
    - 소괄호 안됨
    - 컬리브레이스는 if , else if , else 선언 줄에 작성
    - 조건식은 명확하게 true, false 반환 해야 됨

---
+++ section3/if2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func if2() {
+
+	var a = 50
+	b := 70
+
+	// ex 1
+	if a >= 65 {
+		fmt.Println("65 이상")
+	} else { // else 역시 컬리 브레이스가 else 옆에 있어야 정상 동작함
+		fmt.Println("65 이하")
+	}
+
+	// ex 2
+	if b >= 70 {
+		fmt.Println("70 이상")
+	} else {
+		fmt.Println("70 이하")
+	}
+
+}
---
+++ section3/if3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func if3() {
+	i := 44
+
+	// if - else if 예제 1
+	if i >= 120 {
+		fmt.Println("120 이상")
+	} else if i >= 100 && i < 120 {
+		fmt.Println("100 이상 120 미만")
+	} else if i < 100 && i >= 50 {
+		fmt.Println("50 이상 100 미만")
+	} else {
+		fmt.Println("50 미만")
+	}
+}
