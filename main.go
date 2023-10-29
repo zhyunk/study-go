cc986209f18dc0f8e45969e1325248bf3fd6ae89
zhyunk <gimwlgus@gmail.com>
2023-10-29 20:44:29 +0900

    section5 init

--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"fmt"
-	_ "section1"
-	_ "section2"
-	_ "section3"
-	s4 "section4"
+	// _ "section1"
+	// _ "section2"
+	// _ "section3"
+	// _ "section4"
+	s5 "section5"
 )
 
 func main() {
-	fmt.Println("Root Package - Main Method go go")
-
 	// s1.Test()
 	// s2.Enumiota3()
 	// s3.For3()
-	s4.Init2()
-}
+	// s4.Init2()
 
-func init() {
-	fmt.Println("Root Package - Init Method go go")
 }
---
+++ section5/go.mod
@@ -0,0 +1,3 @@
+module section5
+
+go 1.21.3
